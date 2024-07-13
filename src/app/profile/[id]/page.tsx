468378e557a0a89396c6c2d9ac0b0010cd380890
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2024-07-14 01:47:23 +0530

    final commit

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 const UserProfile = ( {params}:any) => {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-        <h1>Profile</h1>
+        {/* <h1>Profile Page</h1> */}
         <hr />
         <p className="text-4xl "> Profile page
-        <span className="p-2 ml-2 rounded bg-orange-500 text-black">{params.id}</span>
+        <span className="p-3 ml-2 rounded bg-orange-500 text-black">{params.id}</span>
         </p>
     </div>
   )
